fcfe33626ff0451162cbf3cb20c68360a932896d
yuuneeell <ecjoyosa@up.edu.ph>
2025-02-26 11:57:15 +0800

    chore: initial commit for index.js and test.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// JOYOSA, EUNEL JACOB C. | 2023-00895
+// CMSC 100 C-1L
+// EXER 3 - JavaScrip Basics Part 1
+
+function generateUniqueID(fName, lName){
+
+}
+
+const addAccount = (fName, lName, email, age) => {
+
+}
